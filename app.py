c330fb02c88c3466f0a9ec3159b27ed348f7317c
unknown <ha3023032@gmail.com>
2021-06-26 12:34:22 +0900

    6.26

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'omok'
+    return render_template('index.html')
 
 @app.route('/search')
 def search():
     return 'search'
+
+@app.route('/no')
+def no():
+    return 'no'
 
 if __name__ == '__main__':
     app.run(debug=True)
